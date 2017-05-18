3c778352ffb1dde39849b41edfd6be0c96ffa652
Marcel Bezerra da Silva <gft.marcel.silva@br.experian.com>
2017-05-18 14:09:34 -0300

    Criando a biblioteca

---
+++ src/Produto/Manager.php
@@ -0,0 +1,14 @@
+<?php
+namespace Ecompassaro\Produto\Factory;
+
+use Zend\ServiceManager\Factory\FactoryInterface;
+use Interop\Container\ContainerInterface;
+use Ecompassaro\Produto\Manager as Service;
+
+class Manager implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        return new Service($container->get('ProdutoRepository'));
+    }
+}
---
+++ src/Produto/Repository.php
@@ -0,0 +1,18 @@
+<?php
+namespace Ecompassaro\Produto\Factory;
+
+use Zend\ServiceManager\Factory\FactoryInterface;
+use Zend\Db\Adapter\Adapter;
+use Interop\Container\ContainerInterface;
+use Ecompassaro\Produto\Repository as Service;
+use Ecompassaro\Produto\Hydrator;
+use Ecompassaro\Produto\Produto;
+
+class Repository implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $config = $container->get('config');
+        return new Service(new Adapter($config['db']), new Hydrator(), new Produto());
+    }
+}
